443ee70ed4a592d811e6488f0fbe26ded63d2ad9
weiqiang165260 <mushangwq@163.com>
2017-08-09 18:06:49 +0800

    代码删除

--- src/routes/User.js
+++ src/routes/User.js
@@ -4,34 +4,18 @@
 import React,{Component,PropTypes} from "react";
 import {connect} from 'dva';
 import {WingBlank,WhiteSpace} from "antd-mobile";
-import UserList from "../components/Users/UserList"
-import UserSearch from "../components/Users/UserSearch"
-import UserModal from "../components/Users/UserModal"
 
 import style from "./User.css";
 
 function Users({location,dispatch,users}) {
   const {loading,list,total,current,currentItem,modalVisible,modalType} =users;
 
-  const userSearchProps = {};
-  const userListProps = {
-    total,
-    current,
-    loading,
-    dataSource:list
-  };
-  const userModalProps = {};
-
-
   return(
   <div className={style.normal}>
 
     <div className={style.box}>
-      <UserSearch {...userSearchProps}/>
     </div>
-    {/*<WhiteSpace/>*/}
-    <UserList {...userListProps}/>
-    <UserModal {...userModalProps}/>
+    <WhiteSpace/>
   </div>
   );
 }
